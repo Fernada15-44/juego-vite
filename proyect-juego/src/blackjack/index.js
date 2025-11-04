8db3434baa1b47c2d3f3dd4ae40105c0c503aef3
Mariana Fernanda Ramirez Morales <ferramirez1530@gmail.com>
2025-11-04 15:51:03 -0600

    feat: se agrega la funcion exportada de valorCarta

--- proyect-juego/src/blackjack/index.js
+++ proyect-juego/src/blackjack/index.js
@@ -29,15 +29,6 @@ deck = crearDeck(tipos, especiales);
 
 
 
-// ===============================
-// Obtener valor de la carta
-// ===============================
-const valorCarta = (carta) => {
-    const valor = carta.substring(0, carta.length - 1);
-    return (isNaN(valor))
-        ? (valor === 'A') ? 11 : 10
-        : valor * 1;
-}
 
 // ===============================
 // Turno de la computadora
