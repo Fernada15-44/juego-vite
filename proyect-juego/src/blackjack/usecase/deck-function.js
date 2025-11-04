565515aaef77be26bdcd042b37a0f9ac5f047381
Mariana Fernanda Ramirez Morales <ferramirez1530@gmail.com>
2025-11-03 19:59:04 -0600

    fix:se agregaron complementos de documentacion de la funcion.

--- proyect-juego/src/blackjack/usecase/deck-function.js
+++ proyect-juego/src/blackjack/usecase/deck-function.js
@@ -2,13 +2,14 @@
 import _ from 'underscore';
 /**
  * 
- * @param {array} tiposDeCarta 
- * @param {array} tiposEspeciales 
- * @returns {array} retorna un nuevo deck de cartas
+ * @param {array<String>} tiposDeCarta Ejemplo: ['C','D','H','S']
+ * @param {array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {array<String>} retorna un nuevo deck de cartas
  */
 
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
-    if ( !tiposDeCarta ) throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
+    if ( !tiposDeCarta ||tiposDeCarta.lebgth ===0 ) 
+        throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
     let deck = [];
 
     for( let i = 2; i <= 10; i++ ) {
